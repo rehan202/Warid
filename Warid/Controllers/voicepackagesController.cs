93892ceeaa426f104fd47b5549f7f3031febee5a
muhammadmukarram <mukarramsardar@outlook.com>
2016-04-19 11:34:21 +0500

    Mukram Commit

---
+++ Warid/Controllers/voicepackagesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Warid.Controllers
+{
+    public class voicepackagesController : Controller
+    {
+        // GET: voicepackages
+        public ActionResult pakistanoffer()
+        {
+            return View();
+        }
+        public ActionResult hourlypackage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Warid/Controllers/welcomeofferController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Warid.Controllers
+{
+    public class welcomeofferController : Controller
+    {
+        public ActionResult acquisitionmnp()
+        {
+            return View();
+        }
+        public ActionResult ltesimlagao()
+        {
+            return View();
+        }
+        public ActionResult reversesimonline()
+        {
+            return View();
+        }
+        
+
+
+
+    }
+}
