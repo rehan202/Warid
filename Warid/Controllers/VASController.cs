b20f74242a77003d1767cd7ee45e4209746d04ec
rehan202 <rehanshafiq200@live.com>
2016-04-19 13:23:25 +0500

    VAS

---
+++ Warid/Controllers/VASController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Warid.Controllers
+{
+    public class VASController : Controller
+    {
+        // GET: VAS
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult retailersahoolatservices()
+        {
+            return View();
+        }
+        public ActionResult bimainsurance()
+        {
+            return View();
+        }
+        public ActionResult biometricsimreverification()
+        {
+            return View();
+        }
+        public ActionResult entertainment()
+        {
+            return View();
+        }
+        public ActionResult infotainment()
+        {
+            return View();
+        }
+        public ActionResult music()
+        {
+            return View();
+        }
+        public ActionResult religion()
+        {
+            return View();
+        }
+        public ActionResult balancetransferservices()
+        {
+            return View();
+        }
+        public ActionResult utilities()
+        {
+            return View();
+        }
+        public ActionResult mobileapps()
+        {
+            return View();
+        }
+        public ActionResult sports()
+        {
+            return View();
+        }
+        public ActionResult socialnetworking()
+        {
+            return View();
+        }
+    }
+}
---
+++ Warid/Controllers/entertainmentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Warid.Controllers
+{
+    public class entertainmentController : Controller
+    {
+        // GET: entertainment
+        public ActionResult waridlive()
+        {
+            return View();
+        }
+        public ActionResult waridradiostation()
+        {
+            return View();
+        }
+        public ActionResult waridvoiler()
+        {
+            return View();
+        }
+        public ActionResult dramaline()
+        {
+            return View();
+        }
+    }
+}
