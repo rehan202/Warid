a83ab26d5d024397349150b0ab68133aad504db8
03344424475 <m_ali1240@yahoo.com>
2016-04-21 16:06:04 +0500

    music

---
+++ Warid/Controllers/callertunesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Warid.Controllers
+{
+    public class callertunesController : Controller
+    {
+        // GET: callertunes
+        public ActionResult autodialer()
+        {
+            return View();
+        }
+
+        public ActionResult callertunes()
+        {
+            return View();
+        }
+
+        public ActionResult featuresandcharging()
+        {
+            return View();
+        }
+
+        public ActionResult mrbtmovieoffer()
+        {
+            return View();
+        }
+
+        public ActionResult smscommands()
+        {
+            return View();
+        }
+
+        public ActionResult songsearch()
+        {
+            return View();
+        }
+
+        public ActionResult waridnametune()
+        {
+            return View();
+        }
+
+        public ActionResult termsconditions()
+        {
+            return View();
+        }
+    }
+}
---
+++ Warid/Controllers/musicController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Warid.Controllers
+{
+    public class musicController : Controller
+    {
+        // GET: music
+
+         
+        public ActionResult backgroundmusic()
+        {
+            return View();
+        }
+
+        public ActionResult callertunes()
+        {
+            return View();
+        }
+
+        public ActionResult musicstation()
+        {
+            return View();
+        }
+
+
+        public ActionResult waridmusicclub()
+        {
+            return View();
+        }
+
+
+        public ActionResult waridmusicstream()
+        {
+            return View();
+        }
+
+
+        public ActionResult waridrockstar()
+        {
+            return View();
+        }
+    }
+}
